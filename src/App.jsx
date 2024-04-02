8d535f9ac189ba89691675df134b6e7471eaef97
lashasaata <lashasaata@gmail.com>
2024-04-02 13:00:46 -0700

    install axios import shadow and modify app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import HeadingBar from "./components/HeadingBar";
 import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const [userData, setUserData] = useState({});
   return (
-    <div className="h-screen flex flex-col items-center">
-      <HeadingBar />
+    <div className="h-screen flex flex-col items-center bg-[#f6f8ff]">
+      <HeadingBar setUserData={setUserData} />
+      <UserProfile userData={userData} />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    boxShadow: {
+      light: "0 16px 30px -10px rgba(70, 96, 187, 0.2)",
+    },
   },
   plugins: [],
 };
