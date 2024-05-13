7bfa3a6dad9657ffe1377075b1e00636a5645b1e
lashasaata <lashasaata@gmail.com>
2024-05-13 08:05:50 -0700

    feat: add localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import HeadingBar from "./components/HeadingBar";
 import "./App.css";
 import UserProfile from "./components/UserProfile";
 
 function App() {
   const [userData, setUserData] = useState({});
+  const storedData = localStorage.getItem("userData");
+
+  useEffect(() => {
+    localStorage.setItem("userData", JSON.stringify(userData));
+  }, [userData]);
+
+  useEffect(() => {
+    // Parse storedData back to an object when component mounts
+    if (storedData) {
+      setUserData(JSON.parse(storedData));
+    }
+  }, []); // This useEffect runs only once when the component mounts
+
   return (
     <div className="h-screen flex flex-col items-center bg-[#f6f8ff]">
       <HeadingBar setUserData={setUserData} />
